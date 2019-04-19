6168ed10a2d59a3c9c6124346ad179603bf85aed
rosa <rosa.calmopablo@ousd.us>
2019-04-19 01:19:00 +0000

    format

--- js/script.js
+++ js/script.js
@@ -6,3 +6,28 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+var userInput = $('#search').val();
+var soundcloud = "https://api.soundcloud.com/tracks?q=" + userInput + "&client_id=5aa8e389ba4e24b6106af5159ab3e344";
+$("#searchButton").click(function(){
+    $.ajax({
+   url: soundcloud,
+   method: "GET",
+   success: function(response){
+       $("#secondBody").append('<a href="' + response[0].permalink_url  + '">hi</a>');
+        }
+    });
+});
+soundcloud.initialize({
+  client_id: '5aa8e389ba4e24b6106af5159ab3e344'
+});
+
+// stream track id 293
+soundcloud.stream('/tracks/293').then(function(player){
+  player.play().then(function(){
+    console.log('Playback started!');
+  }).catch(function(e){
+    console.error('Playback rejected. Try calling play() from a user interaction.', e);
+  });
+});
+
+// I don't understand any of the javascript code lol
