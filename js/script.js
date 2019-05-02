6f1ab05c6dacb43d693dc7b982c65fc1143640cb
rosa <rosa.calmopablo@ousd.us>
2019-05-02 07:12:52 +0000

    Attempted to add search results

--- js/script.js
+++ js/script.js
@@ -10,28 +10,17 @@ var userInput = $('#search').val();
 var soundcloud = "https://api.soundcloud.com/tracks?q=" + userInput + "&client_id=5aa8e389ba4e24b6106af5159ab3e344";
 
 $("#searchButton").click(function(){
+    
     $.ajax({
    url: soundcloud,
    method: "GET",
    success: function(response){
-       $("#secondBody").append('<a href="' + response[0].permalink_url  + '">hi</a>');
+       $("#searchedResults").show();
+       $("#songTitle").html('<p>' + response[0].title + '</p>');
         
-       console.log("hi");
    }
-        
+    
     });
-});
-SC.initialize({
- client_id: '5aa8e389ba4e24b6106af5159ab3e344'
-});
-
-// stream track id 293
-SC.stream('/tracks/293').then(function(player){
-  player.play().then(function(){
-    console.log('Playback started!');
-  }).catch(function(e){
-    console.error('Playback rejected. Try calling play() from a user interaction.', e);
-  });
 });
 
 // I don't understand any of the javascript code lol
